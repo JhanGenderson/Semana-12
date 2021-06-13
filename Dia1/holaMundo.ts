0a096f83b79ce26dc8a55d483ef85068a8010f01
JhanGenderson <jhan96.rc@gmail.com>
2021-06-12 20:03:19 -0500

    typescript

---
+++ Dia1/holaMundo.ts
@@ -0,0 +1,18 @@
+let saludo = "Hola Mundo"
+
+console.log(saludo)
+
+let texto:string ="Conferencia Xbox"
+
+// texto = 100 error tipo numero no puede ser asignado a un string
+
+
+let cantidad:number = 100
+
+let losQuiere:boolean = true
+
+let misNumeritos:Array<number>=[20,18,19,17,20,12]
+
+let random:any = {nombre:"Balto"}
+
+let random2:unknown = {nombre:"Luna"}
---
+++ Dia1/interfaz.ts
@@ -0,0 +1,13 @@
+//interface => contrato
+
+interface iGato{
+    nombre:string
+    edad:number
+    raza:string
+}
+
+let gatita:iGato={
+    nombre:'gato',
+    edad:6,
+    raza:"Siames"
+}
